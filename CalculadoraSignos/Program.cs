2372512cc939574989359fcf6ea196c10446872b
André Felipe <andrefeliperf17@gmail.com>
2020-12-23 16:35:43 -0300

    Calculadora de signos

---
+++ CalculadoraSignos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraSignos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dia, mes;
+            int diaInt, mesInt;
+
+            InterpretadorSigno interpretadorSigno = new InterpretadorSigno();
+
+            Console.WriteLine("Dia de nascimento");
+            dia = Console.ReadLine();
+            Console.WriteLine("Mês de nascimento");
+            mes = Console.ReadLine();
+
+            diaInt = Convert.ToInt32(dia);
+            mesInt = Convert.ToInt32(mes);
+
+            Signo signo = interpretadorSigno.Interpretar(diaInt, mesInt);
+            if (signo != null)
+            {
+                Console.WriteLine($"Seu signo é {signo.nome}\nCaracteristica: {signo.caracteristicas}");
+            } else
+            {
+                Console.WriteLine("Data invalidas");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CalculadoraSignos/Signo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraSignos
+{
+    class Signo
+    {
+        public string nome;
+        public int diaInicio, mesInicio, diaFim, mesFim;
+        public string caracteristicas;
+    }
+}
